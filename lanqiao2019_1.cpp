4714862b6d60dff04693080977b4b0abe4e4fbc5
Li-Hongcheng <48978411+Li-Hongcheng@users.noreply.github.com>
2020-02-10 20:02:58 +0800

    1

--- lanqiao2019_1.cpp
+++ lanqiao2019_1.cpp
@@ -1,18 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-bool check(int num){
-	while(num){
-		if(num%10==2||num%10==0||num%10==1||num%10==9) return true;
-		num/=10;
-	}
-	return false;
+bool check(int num)
+{
+    while (num)
+    {
+        if (num % 10 == 2 || num % 10 == 0 || num % 10 == 1 || num % 10 == 9)
+            return true;
+        num /= 10; 
+
+    }
+    return false;
 }
-long long ans=0;
+long long ans = 0;
 int main()
 {
-	for(int i=1;i<=2019;i++){
-		if(check(i)) ans+=i*i;
-	}
-	cout<<ans<<endl;
-	return 0;
- } 
+    for (int i = 1; i <= 2019; i++)
+    {
+        if (check(i))
+            ans += i * i;
+    }
+    cout << ans << endl;
+}
